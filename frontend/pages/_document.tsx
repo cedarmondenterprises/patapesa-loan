73f5968b78357bd9f45f37c08030f26c9fdaa905
cedarmondenterprises <cedarmondenterprises@gmail.com>
2026-09-15 08:45:52 +0300

    Add Google AdSense ownership verification meta tag

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en-KE">
       <Head>
         <meta name="format-detection" content="telephone=no" />
+        <meta name="google-adsense-account" content="ca-pub-3552158702367587" />
       </Head>
       <body><Main /><NextScript /></body>
     </Html>
